4a844f197744ff2a8a0ec74dabe3fca8ee0f8c41
ldinghan <86869693+ldinghan@users.noreply.github.com>
2022-04-21 19:19:50 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const introTexts = document.querySelectorAll(".intro_text");
+const currentIntroText = document.querySelector("#currentIntroText");
+
+
+const displayIntro = () => {
+	currentIntroText.classList.add('loading');
+	const type = (i, t, ie) => {
+	    input = ie.innerHTML;
+	    currentIntroText.innerHTML += input.charAt(i);
+	    setTimeout(function(){
+	        ((i < input.length - 1) ? type(i+1, t, ie) : false);
+	    }, t);
+	}
+
+	const showNext = (text) => {
+		return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			currentIntroText.innerHTML = "";
+			type(0,100,text)
+			resolve();
+		}, 4000)
+
+		})
+	}
+
+
+	showNext(introTexts[0])
+		.then(() => showNext(introTexts[1]))
+}
+
+displayIntro()
+
+
+
+

4e53aeb7d44a29141609efd5be7a87106aead19e
ldinghan <86869693+ldinghan@users.noreply.github.com>
2022-04-23 16:23:11 +0800

    Update pageFunctions.js

--- scripts/pageFunctions.js
+++ scripts/pageFunctions.js
@@ -52,7 +52,7 @@ const bubbleSpawn = () => {
 		popBubble(newBubble)
 		if (popped === false) {
 			popped = true;
-			alert('Keep exploring to pop more bubbles! More bubbles will spawn as you explore the page!')
+			alert('Keep exploring to pop more bubbles! More bubbles will spawn as you explore the page! (Your progress will be lost once you close your browser)')
 			popScoreDisplay.style.display = 'block';
 			popScoreDisplay.textContent = `Bubbles Popped: ${popScore}`			
 		}
